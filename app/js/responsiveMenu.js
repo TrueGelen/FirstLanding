1551cd24e419357ffcd1f185c675557f90173c75
Vlad Filchagin <gelenbagen@gmail.com>
2019-08-23 14:59:50 +0300

    some changes

--- app/js/responsiveMenu.js
+++ app/js/responsiveMenu.js
@@ -1,14 +1,12 @@
 window.addEventListener('load', function () {
 	console.log("script from responsiveMenu is working")
+
 	const mobMenu = document.querySelector('header .container .menuwrapper .mobMenu')
 	const menu = document.querySelector('header .container .menuwrapper .menu')
 	const headerTitle = document.querySelector('header .container .mainTitle')
 	const btn = document.querySelector('header .container .firstBtn')
 
-	console.log(menu, mobMenu, headerTitle)
-
-	this.console.log(menu.style.display)
-
+	//behavior of MobMenu on click
 	mobMenu.addEventListener('click', function () {
 		if (menu.style.display !== 'block') {
 			menu.style.display = 'block'
@@ -20,5 +18,14 @@ window.addEventListener('load', function () {
 			headerTitle.style.visibility = 'visible'
 			btn.style.visibility = 'visible'
 		}
+	})
+
+	//behavior of menu on resize
+	this.window.addEventListener('resize', function () {
+		this.console.log('this.pageXOffset', this.innerWidth)
+		if (this.innerWidth > 960)
+			menu.style.display = 'block'
+		else
+			menu.style.display = 'none'
 	})
 })
